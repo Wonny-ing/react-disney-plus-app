764d55bfb27000e185e0cbec3f6f9102a57f6e32
Wonny-ing <h.wonny11@gmail.com>
2023-01-04 19:20:59 +0900

    fix: axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://api.themoviedb.org/3',
+  baseURL: 'https://api.themoviedb.org/3',
   params: {
     api_key: 'b18e798ff377ef49f1c335283e7c43d6',
     language: 'ko-KR',
